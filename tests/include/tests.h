58e18fed074ee603e6bcc852bc3f4bd9ff3847dc
Maxwell Babey <babeymax@gmail.com>
2023-01-06 10:40:57 -0800

    Initial commit

---
+++ tests/include/tests.h
@@ -0,0 +1,20 @@
+#ifndef CSHELL_TESTS_H
+#define CSHELL_TEST S_H
+
+#include <cgreen/cgreen.h>
+
+TestSuite *buitlin_tests(void);
+
+TestSuite *command_tests(void);
+
+TestSuite *execute_tests(void);
+
+TestSuite *input_tests(void);
+
+TestSuite *shell_tests(void);
+
+TestSuite *shell_impl_tests(void);
+
+TestSuite *util_tests(void);
+
+#endif //CSHELL_TESTS_H
