88bbbdebf705d069bdb8563b6643cfd247aa581a
Maxwell Babey <babeymax@gmail.com>
2023-01-07 11:45:19 -0800

    Write unit test for get_prompt

--- include/util.h
+++ include/util.h
@@ -8,8 +8,40 @@
 #include <dc_env/env.h>
 #include <dc_error/error.h>
 
+/**
+ * get_prompt
+ * <p>
+ * Get the prompt to use.
+ * </p>
+ * @param env the environment
+ * @param err the error object
+ * @return the value of the PS1 env var or "$" if PS1 not set
+ */
 const char *get_prompt(const struct dc_env *env, struct dc_error *err);
 
+/**
+ * get_path
+ * <p>
+ * Get the PATH environment variable.
+ * </p>
+ * @param env the environment
+ * @param err the error object
+ * @return the PATH env var
+ */
 char *get_path(const struct dc_env *env, struct dc_error *err);
+
+/**
+ * parse_path
+ * <p>
+ * Separate a path, such as the PATH env var, into separate directories.
+ * Directories are separated with a ':' character. Any paths with '~'
+ * are converted to the user's home directory.
+ * </p>
+ * @param env the environment
+ * @param err the error object
+ * @param path_str the path var
+ * @return the directories that make up the path
+ */
+char **parse_path(const struct dc_env *env, struct dc_error *err, char *path_str);
 
 #endif //CSHELL_TESTS_UTIL_H
--- tests/src/util_tests.c
+++ tests/src/util_tests.c
@@ -1,20 +1,23 @@
 #include "../include/tests.h"
 #include "../../include/util.h"
-#include <dc_util/strings.h>
+#include <dc_posix/dc_stdlib.h>
+#include <dc_c/dc_stdlib.h>
+
 
 // NOLINTBEGIN
 
 static void check_state_reset(const struct dc_error error, const struct state state, FILE in, FILE out, FILE err);
+
 static void test_parse_path(const char path_str, char *dirs);
 
 Describe(util);
 
 static struct dc_error *error;
-static struct dc_env *environ;
+static struct dc_env   *environ;
 
 BeforeEach(util)
 {
-    error = dc_error_create(false);
+    error   = dc_error_create(false);
     environ = dc_env_create(error, false, NULL);
 }
 
@@ -23,9 +26,22 @@ AfterEach(util)
     dc_error_reset(error);
 }
 
-Ensure(util, get_path)
+Ensure(util, get_prompt)
 {
-
+    const char *envname = "PS1";
+    const char *envval  = NULL;
+    char       *prompt;
+    
+    prompt = dc_getenv(environ, envname);
+    
+    if (prompt != NULL)
+    {
+        dc_setenv(environ, error, envname, envval, true);
+    }
+    
+    prompt = get_prompt(environ, error);
+    
+    assert_that(prompt, is_equal_to_string("$"));
 }
 
 TestSuite *util_tests(void)
@@ -33,7 +49,7 @@ TestSuite *util_tests(void)
     TestSuite *suite;
     
     suite = create_test_suite();
-    add_test_with_context(suite, util, get_path);
+    add_test_with_context(suite, util, get_prompt);
     
     return suite;
 }
