b1956a48d575fa162b96ed3f46853d0e59d640fb
Maxwell Babey <babeymax@gmail.com>
2023-01-07 11:50:50 -0800

    Add sketch implementation of some util functions in order to test tests

---
+++ src/util.c
@@ -0,0 +1,16 @@
+#include "../include/util.h"
+
+const char *get_prompt(const struct dc_env *env, struct dc_error *err)
+{
+    return "$";
+}
+
+char *get_path(const struct dc_env *env, struct dc_error *err)
+{
+    return NULL;
+}
+
+char **parse_path(const struct dc_env *env, struct dc_error *err, char *path_str)
+{
+    return NULL;
+}
