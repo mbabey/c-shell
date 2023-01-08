27f50e8f702e3bf6e77204c1e1bc579d888a58d7
Maxwell Babey <babeymax@gmail.com>
2023-01-08 15:07:27 -0800

    Add declarations for shell_impl.h functions

--- include/shell_impl.h
+++ include/shell_impl.h
@@ -13,10 +13,11 @@
  * <p>
  * Set up the initial state.
  * </p>
+ *
  * <ul>
- * <li>in_redirect_regex: "[\t\f\v]<.*"</li>
- * <li>out_redirect_regex: "[\t\f\v][1^2]?>[>]?.*"</li>
- * <li>err_redirect_regex: "[\t\f\v]2>[>].*"</li>
+ * <li>in_redirect_regex: "[&#x2216;t&#x2216;f&#x2216;v]<.*"</li>
+ * <li>out_redirect_regex: "[&#x2216;t&#x2216;f&#x2216;v][1^2]?>[>]?.*"</li>
+ * <li>err_redirect_regex: "[&#x2216;t&#x2216;f&#x2216;v]2>[>].*"</li>
  * <li>path: the PATH env var separated into directories</li>
  * <li>prompt: the PS1 env var if set, otherwise "$"</li>
  * <li>max_line_length: the value of _SC_ARG_MAX (see sysconfig)</li>
@@ -39,5 +40,69 @@ int init_state(const struct dc_env *env, struct dc_error *err, void *arg);
  * @return DC_FSM_EXIT
  */
 int destroy_state(const struct dc_env *env, struct dc_error *err, void *arg);
+
+/**
+ * reset_state
+ * <p>
+ * Reset the state for the next read.
+ * </p>
+ * @param env the environment
+ * @param err the error object
+ * @param arg the current struct state
+ * @return READ_COMMANDS
+ */
+int reset_state(const struct dc_env *env, struct dc_error *err, void *arg);
+
+/**
+ * read_commands
+ * <p>
+ * Prompt the user and read the command line. Sets state->current_line and
+ * current_line_length.
+ * </p>
+ * @param env the environment
+ * @param err the error object
+ * @param arg the current struct state
+ * @return SEPARATE_COMMANDS
+ */
+int read_commands(const struct dc_env *env, struct dc_error *err, void *arg);
+
+/**
+ * separate_commands
+ * <p>
+ * Separate the commands. In the current implementation there is only one
+ * command. Sets state->command
+ * </p>
+ * @param env the environment
+ * @param err the error object
+ * @param arg the current struct state
+ * @return PARSE_COMMANDS or SEPARATE_ERROR
+ */
+int separate_commands(const struct dc_env *env, struct dc_error *err, void *arg);
+
+/**
+ * parse_commands
+ * <p>
+ * Parse the commands (see parse_command).
+ * </p>
+ * @param env the environment
+ * @param err the error object
+ * @param arg the current struct state
+ * @return EXECUTE_COMMANDS or PARSE_ERROR
+ */
+int parse_commands(const struct dc_env *env, struct dc_error *err, void *arg);
+
+/**
+ * execute_commands
+ * <p>
+ * Run the command (see execute).
+ * If the command->command is "cd", run builtin_cd.
+ * If the command->command is
+ * </p>
+ * @param env the environment
+ * @param err the error object
+ * @param arg the current struct state
+ * @return EXECUTE_COMMANDS or PARSE_ERROR
+ */
+int execute_commands(const struct dc_env *env, struct dc_error *err, void *arg);
 
 #endif //CSH_SHELL_IMPL_H
