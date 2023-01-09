be02155f0ac26cde023280198cffb9fec68d97fa
Maxwell Babey <babeymax@gmail.com>
2023-01-08 22:21:57 -0800

    Update input.h tests

--- tests/src/input_tests.c
+++ tests/src/input_tests.c
@@ -1,14 +1,14 @@
 #include "../include/tests.h"
 #include "../../include/util.h"
+#include "../../include/input.h"
 #include <dc_env/env.h>
 #include <dc_error/error.h>
 
 // NOLINTBEGIN
 
-static void check_state_reset(const struct dc_error error, const struct state state, FILE in, FILE out, FILE err);
-static void test_parse_path(const char path_str, char *dirs);
-
 Describe(input);
+
+static void test_read_command_line(FILE *strstream, size_t *line_length, const char *str);
 
 static struct dc_error *error;
 static struct dc_env *environ;
@@ -26,7 +26,24 @@ AfterEach(input)
 
 Ensure(input, read_command_line)
 {
+    char str[BUFSIZ];
+    FILE *strstream;
+    size_t line_length;
+    
+    strstream = fmemopen(str, sizeof(str), "r");
+    strcpy(str, "hello\n");
+    line_length = 10;
+    test_read_command_line(strstream, &line_length, "hello\n");
+}
 
+static void test_read_command_line(FILE *strstream, size_t *line_length, const char *expected_line)
+{
+    char *line;
+    
+    line = read_command_line(environ, error, strstream, NULL, line_length);
+    
+    assert_that(line, is_equal_to_string(expected_line));
+    assert_that(*line_length, is_equal_to(strlen(line)));
 }
 
 TestSuite *input_tests(void)
