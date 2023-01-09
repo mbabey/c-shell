67a15075549e646369680eb9dae704ec1523448d
Maxwell Babey <babeymax@gmail.com>
2023-01-08 18:37:18 -0800

    Add tests for shell_impl

--- tests/src/shell_impl_tests.c
+++ tests/src/shell_impl_tests.c
@@ -28,12 +28,61 @@ Ensure(shell_impl, init_state)
 
 }
 
+Ensure(shell_impl, destroy_state)
+{
+
+}
+
+Ensure(shell_impl, reset_state)
+{
+
+}
+
+Ensure(shell_impl, read_commands)
+{
+
+}
+
+Ensure(shell_impl, separate_commands)
+{
+
+}
+
+Ensure(shell_impl, parse_commands)
+{
+
+}
+
+Ensure(shell_impl, execute_commands)
+{
+
+}
+
+Ensure(shell_impl, do_exit)
+{
+
+}
+
+Ensure(shell_impl, handle_error)
+{
+
+}
+
+
 TestSuite *shell_impl_tests(void)
 {
     TestSuite *suite;
     
     suite = create_test_suite();
     add_test_with_context(suite, shell_impl, init_state);
+    add_test_with_context(suite, shell_impl, destroy_state);
+    add_test_with_context(suite, shell_impl, reset_state);
+    add_test_with_context(suite, shell_impl, read_commands);
+    add_test_with_context(suite, shell_impl, separate_commands);
+    add_test_with_context(suite, shell_impl, parse_commands);
+    add_test_with_context(suite, shell_impl, execute_commands);
+    add_test_with_context(suite, shell_impl, do_exit);
+    add_test_with_context(suite, shell_impl, handle_error);
     
     return suite;
 }
