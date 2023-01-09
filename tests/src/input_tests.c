5ca6fc0e5187976f702a40c0009073655a3d4f37
Maxwell Babey <babeymax@gmail.com>
2023-01-09 09:51:51 -0800

    define test for read_command_line

--- tests/src/input_tests.c
+++ tests/src/input_tests.c
@@ -3,19 +3,20 @@
 #include "../../include/input.h"
 #include <dc_env/env.h>
 #include <dc_error/error.h>
+#include <dc_c/dc_stdlib.h>
 
 // NOLINTBEGIN
 
 Describe(input);
 
-static void test_read_command_line(FILE *strstream, size_t *line_length, const char *str);
+static void test_read_command_line(const char *initial_line, ...);
 
 static struct dc_error *error;
-static struct dc_env *environ;
+static struct dc_env   *environ;
 
 BeforeEach(input)
 {
-    error = dc_error_create(false);
+    error   = dc_error_create(false);
     environ = dc_env_create(error, false, NULL);
 }
 
@@ -26,24 +27,58 @@ AfterEach(input)
 
 Ensure(input, read_command_line)
 {
-    char str[BUFSIZ];
-    FILE *strstream;
-    size_t line_length;
-    
-    strstream = fmemopen(str, sizeof(str), "r");
-    strcpy(str, "hello\n");
-    line_length = 10;
-    test_read_command_line(strstream, &line_length, "hello\n");
+    test_read_command_line("z\n", "z", NULL);
+    test_read_command_line("hello\nworld\n", "hello", "world", NULL);
+    test_read_command_line("  hello  \n", "hello", NULL);
+    test_read_command_line("  cruel gagoonga\t\n", "cruel gagoonga", NULL);
+    test_read_command_line("./a.out hello < in.txt > out.txt 2> err.txt\n",
+                           "./a.out hello < in.txt > out.txt 2> err.txt", NULL);
 }
 
-static void test_read_command_line(FILE *strstream, size_t *line_length, const char *expected_line)
+static void test_read_command_line(const char *initial_line, ...)
 {
-    char *line;
+    char    *str;
+    char    *expected_line;
+    FILE    *strstream;
+    va_list strings;
+    size_t  line_length;
     
-    line = read_command_line(environ, error, strstream, NULL, line_length);
+    line_length = strlen(initial_line + 1);
+    str         = dc_malloc(environ, error, line_length);
     
-    assert_that(line, is_equal_to_string(expected_line));
-    assert_that(*line_length, is_equal_to(strlen(line)));
+    strstream = fmemopen(str, sizeof(str), "r");
+    strcpy(str, initial_line);
+    
+    // va loop
+    va_start(strings, initial_line);
+    
+    do
+    {
+        char *line;
+        
+        line = read_command_line(environ, error, strstream, NULL, &line_length);
+        
+        expected_line = va_arg(strings, char *);
+        
+        if (expected_line == NULL)
+        {
+            assert_that(line, is_equal_to_string(""));
+            assert_that(line_length, is_equal_to(0));
+        } else
+        {
+            assert_that(line, is_equal_to_string(expected_line));
+            assert_that(line_length, is_equal_to(strlen(line)));
+        }
+    } while (expected_line);
+    
+    va_end(strings);
+    
+    fclose(strstream);
+    dc_free(environ, str);
+}
+
+static void do_test_read_command_line(FILE *strstream, size_t *line_length, const char *expected_line)
+{
 }
 
 TestSuite *input_tests(void)
