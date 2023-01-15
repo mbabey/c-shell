8cd8cd872a1de7da3ab493cbafee3c015817d6cd
Maxwell Babey <babeymax@gmail.com>
2023-01-14 20:32:39 -0800

    Update execute test

--- tests/src/execute_tests.c
+++ tests/src/execute_tests.c
@@ -3,9 +3,16 @@
 #include "../../include/execute.h"
 #include "../../include/supervisor.h"
 #include "util.h"
+#include <sys/stat.h>
+#include <unistd.h>
+
 // NOLINTBEGIN
 
-static void test_execute(const char *cmd, char **path, char **argv, int expected_exit_code, const char *out_file_name);
+static void
+test_execute(const char *cmd, char **path, size_t argc, char **argv, bool check_exit_code, int expected_exit_code,
+             const char *out_file_name, const char *err_file_name);
+
+static void check_redirection(const char *file_name);
 
 Describe(execute);
 
@@ -39,43 +46,86 @@ Ensure(execute, execute)
 {
     char **path;
     char **argv;
+    char template[16];
     
     path = dc_strs_to_array(supvis->env, supvis->err, 3, "/bin", "/usr/bin", NULL);
     
     argv = dc_strs_to_array(supvis->env, supvis->err, 2, NULL, NULL);
-    test_execute("pwd", path, NULL, 0, NULL);
+    test_execute("pwd", path, 1, argv, true, 0, NULL, NULL);
     dc_strs_destroy_array(supvis->env, 2, argv);
     
     argv = dc_strs_to_array(supvis->env, supvis->err, 2, NULL, NULL);
-    test_execute("ls", path, NULL, 0, NULL);
+    strcpy(template, "/tmp/fileXXXXXX");
+    test_execute("ls", path, 1, argv, true, 0, template, NULL);
     dc_strs_destroy_array(supvis->env, 2, argv);
+    
+    argv = dc_strs_to_array(supvis->env, supvis->err, 3, NULL, "adsafsdfasfsdsafgsdfsdadssdfad", NULL);
+    strcpy(template, "/tmp/fileXXXXXX");
+    test_execute("ls adsafsdfasfsdsafgsdfsdadssdfad", path, 2, argv, true, 1, NULL, template);
+    dc_strs_destroy_array(supvis->env, 3, argv);
     
     dc_strs_destroy_array(supvis->env, 3, path);
     path = dc_strs_to_array(supvis->env, supvis->err, 1, NULL);
     argv = dc_strs_to_array(supvis->env, supvis->err, 2, NULL, NULL);
-    test_execute("ls", path, NULL, 127, NULL);
+    test_execute("ls", path, 1, argv, false, ENOENT, NULL, NULL);
     dc_strs_destroy_array(supvis->env, 2, argv);
     dc_strs_destroy_array(supvis->env, 1, path);
     
     dc_strs_destroy_array(supvis->env, 3, path);
     path = dc_strs_to_array(supvis->env, supvis->err, 2, "/", NULL);
     argv = dc_strs_to_array(supvis->env, supvis->err, 2, NULL, NULL);
-    test_execute("ls", path, NULL, 127, NULL);
+    test_execute("ls", path, 1, argv, true, 127, NULL, NULL);
     dc_strs_destroy_array(supvis->env, 2, argv);
     dc_strs_destroy_array(supvis->env, 2, path);
 }
 
-static void test_execute(const char *cmd, char **path, char **argv, int expected_exit_code, const char *out_file_name)
+static void
+test_execute(const char *cmd, char **path, size_t argc, char **argv, bool check_exit_code, int expected_exit_code,
+             const char *out_file_name, const char *err_file_name)
 {
     struct command command;
     
     memset(&command, 0, sizeof(struct command));
     command.command = strdup(cmd);
+    command.argc    = argc;
     command.argv    = argv;
+    
+    if (out_file_name)
+    {
+        command.stdout_file = strdup(out_file_name);
+    }
+    if (err_file_name)
+    {
+        command.stderr_file = strdup(err_file_name);
+    }
+    
     execute(supvis, &command, path);
-    assert_that(command.exit_code, is_equal_to(expected_exit_code));
+    
+    if (expected_exit_code)
+    {
+        assert_that(command.exit_code, is_equal_to(expected_exit_code));
+    }
+    
+    check_redirection(out_file_name);
+    check_redirection(err_file_name);
+    
     do_reset_command(supvis, &command);
     free(command.command);
+}
+
+static void check_redirection(const char *file_name)
+{
+    if (file_name)
+    {
+        struct stat st;
+        int         status;
+        
+        status = stat(file_name, &st);
+        assert_that(status, is_equal_to(0));
+        assert_that(st.st_size, is_greater_than(0));
+        
+        unlink(file_name);
+    }
 }
 
 TestSuite *execute_tests(void)
