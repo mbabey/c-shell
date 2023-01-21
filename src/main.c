5e47313f0b34e1d3af185e202925f8bb155fe22d
Maxwell Babey <babeymax@gmail.com>
2023-01-20 19:34:08 -0800

    Implement switch statement to handle program states

--- src/main.c
+++ src/main.c
@@ -7,20 +7,67 @@
 int main(void)
 {
     struct supervisor *supvis = init_supervisor();
-    struct state state;
-    int next_state;
+    struct state      state;
+    int               next_state;
+    int               run;
     
     next_state = init_state(supvis, &state);
     
-    if (next_state == READ_COMMANDS)
+    run = 1;
+    while (run)
     {
-        next_state = read_commands(supvis, &state);
-    } else if (next_state == ERROR)
-    {
-        next_state = handle_error(supvis, &state);
+        switch (next_state)
+        {
+            case READ_COMMANDS:
+            {
+                next_state = read_commands(supvis, &state);
+                break;
+            }
+            case SEPARATE_COMMANDS:
+            {
+                next_state = separate_commands(supvis, &state);
+                break;
+            }
+            case PARSE_COMMANDS:
+            {
+                next_state = parse_commands(supvis, &state);
+                break;
+            }
+            case EXECUTE_COMMANDS:
+            {
+                next_state = execute_commands(supvis, &state);
+                break;
+            }
+            case RESET_STATE:
+            {
+                next_state = reset_state(supvis, &state);
+                break;
+            }
+            case DESTROY_STATE:
+            {
+                next_state = destroy_state(supvis, &state);
+                break;
+            }
+            case ERROR:
+            {
+                next_state = handle_error(supvis, &state);
+                break;
+            }
+            case EXIT:
+            {
+                next_state = do_exit(supvis, &state);
+                run = 0;
+                break;
+            }
+            default:
+            {
+                run = 0; // Should never get here.
+                break;
+            }
+        }
     }
-
+    
     destroy_supervisor(supvis);
-
+    
     return EXIT_SUCCESS;
 }
