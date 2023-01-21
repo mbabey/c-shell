86ca7afb7d441e7ffd39d1bae59dc9db1726f3f2
Maxwell Babey <babeymax@gmail.com>
2023-01-20 18:51:54 -0800

    Refactor execute function

--- src/execute.c
+++ src/execute.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <unistd.h>
 
+int do_execute(struct state *state, struct command *command, char *const *path, size_t cmd_len, int status);
+
 int do_execute_commands(struct supervisor *supvis, struct state *state)
 {
     int ret_val;
@@ -26,33 +28,41 @@ int execute(struct supervisor *supvis, struct state *state, struct command *comm
     } else
     {
         size_t cmd_len;
-        size_t len;
         int    status;
-        char   *path_and_cmd;
         
         cmd_len = strlen(command->command);
         
+        status = 1;
         for (; *path || status == 0; ++path)
         {
-            len          = strlen(*path) + cmd_len + 1;
-            path_and_cmd = (char *) malloc(len);
-            strcpy(path_and_cmd, *path);
-            strcat(path_and_cmd, command->command);
-            
-            status = execv(path_and_cmd, command->argv);
-            if (status == -1 && errno == ENOENT)
-            {
-                errno = 0;
-            } else if (status == -1)
-            {
-                state->fatal_error = true;
-            }
-            
-            free(path_and_cmd);
+            status = do_execute(state, command, path, cmd_len, status);
         }
         
         ret_val = (state->fatal_error) ? ERROR : RESET_STATE;
     }
     
     return ret_val;
+}
+
+int do_execute(struct state *state, struct command *command, char *const *path, size_t cmd_len, int status)
+{
+    size_t len;
+    char   *path_and_cmd;
+    
+    len          = strlen(*path) + cmd_len + 1;
+    path_and_cmd = (char *) malloc(len);
+    strcpy(path_and_cmd, *path);
+    strcat(path_and_cmd, command->command);
+    
+    status = execv(path_and_cmd, command->argv);
+    if (status == -1 && errno == ENOENT)
+    {
+        errno = 0;
+    } else if (status == -1)
+    {
+        state->fatal_error = true;
+    }
+    
+    free(path_and_cmd);
+    return status;
 }
