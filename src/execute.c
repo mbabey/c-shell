cd4cb90f933067c9986236285804d84f96d53471
Maxwell Babey <babeymax@gmail.com>
2023-01-21 14:05:45 -0800

    Define error message and exit code for all possible execv errors

--- src/execute.c
+++ src/execute.c
@@ -4,6 +4,16 @@
 #include <string.h>
 #include <unistd.h>
 
+#define EXIT_EACCES 4
+#define EXIT_EFAULT 5
+#define EXIT_EINVAL 6
+#define EXIT_ENOTDIR 7
+#define EXIT_ELOOP 8
+#define EXIT_ENAMETOOLONG 9
+#define EXIT_ENOEXEC 10
+#define EXIT_E2BIG 69
+#define EXIT_ENOENT 127
+
 /**
  * exec_command
  * <p>
@@ -44,10 +54,11 @@ void child_parse_path_exec(struct supervisor *supvis, struct state *state, struc
  * get_exit_code
  * <p>
  * Get an exit code for the child process based on the result of running execv.
+ * Print an error message to state->stderr based on the set errno.
  * </p>
  * @return the exit code
  */
-int get_exit_code(void);
+int get_exit_code(const char *command, FILE *err);
 
 /**
  * parent_wait
@@ -84,7 +95,7 @@ int execute(struct supervisor *supvis, struct state *state, struct command *comm
     } else
     {
         fork_and_exec(supvis, state, command, path);
-    
+        
         ret_val = (state->fatal_error) ? ERROR : RESET_STATE;
     }
     
@@ -113,7 +124,7 @@ void child_parse_path_exec(struct supervisor *supvis, struct state *state, struc
 {
     size_t cmd_len;
     int    status;
-    int exit_code;
+    int    exit_code;
     
     cmd_len = strlen(command->command);
     
@@ -126,14 +137,12 @@ void child_parse_path_exec(struct supervisor *supvis, struct state *state, struc
     supvis->mm->mm_free_all(supvis->mm);
     free(supvis);
     
-    exit_code = get_exit_code();
+    exit_code = get_exit_code(command->command, state->stderr);
     
     exit(exit_code);
 }
 
-#define EXNOTFOUND 127
-
-int get_exit_code(void)
+int get_exit_code(const char *command, FILE *err)
 {
     int exit_code;
     
@@ -141,21 +150,74 @@ int get_exit_code(void)
     {
         case ENOENT:
         {
-            exit_code = EXNOTFOUND;
+            exit_code = EXIT_ENOENT;
+            fprintf(err, "csh: command not found: %s", command);
+            break;
+        }
+        case EACCES:
+        {
+            exit_code = EXIT_EACCES;
+            fprintf(err, "csh: permission denied: %s", command);
+            break;
+        }
+        case EFAULT:
+        {
+            exit_code = EXIT_EFAULT;
+            fprintf(err, "csh: arguments outside address space: %s", command);
+            break;
+        }
+        case EINVAL:
+        {
+            exit_code = EXIT_EINVAL;
+            fprintf(err, "csh: execution not supported by this system: %s", command);
+            break;
+        }
+        case ENOTDIR:
+        {
+            exit_code = EXIT_ENOTDIR;
+            fprintf(err, "csh: no such file or directory: %s", command);
+            break;
+        }
+        case ELOOP:
+        {
+            exit_code = EXIT_ELOOP;
+            fprintf(err, "csh: symbolic loop detected: %s", command);
+            break;
+        }
+        case ENAMETOOLONG:
+        {
+            exit_code = EXIT_ENAMETOOLONG;
+            fprintf(err, "csh: argument name too long: %s", command);
+            break;
+        }
+        case ENOEXEC:
+        {
+            exit_code = EXIT_ENOEXEC;
+            fprintf(err, "csh: unrecognized format: %s", command);
+            break;
+        }
+        case E2BIG:
+        {
+            exit_code = EXIT_E2BIG;
+            fprintf(err, "csh: too many arguments and/or environment variables: %s", command);
             break;
         }
         default:
         {
-            exit_code =
+            exit_code = EXIT_FAILURE;
+            fprintf(err, "csh: undefined error: %s", command);
+            break;
         }
     }
+    
+    return exit_code;
 }
 
 int exec_command(struct state *state, struct command *command, char *const *path, size_t cmd_len)
 {
     size_t len;
     char   *path_and_cmd;
-    int status;
+    int    status;
     
     len          = strlen(*path) + cmd_len + 2;
     path_and_cmd = (char *) malloc(len);
@@ -178,7 +240,7 @@ int exec_command(struct state *state, struct command *command, char *const *path
 void parent_wait(const struct supervisor *supvis, struct state *state, struct command *command, pid_t pid)
 {
     pid_t wait_ret;
-    int ret_val;
+    int   ret_val;
     
     wait_ret = waitpid(pid, &ret_val, 0);
     if (wait_ret == -1)
